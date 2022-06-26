18c9045e0e284a357ff1b14465d5c91fa6385c97
vov4ik74rus93 <vov4ik74rus93@gmail.com>
2022-06-27 01:21:15 +0500

    ...

--- src/Block.java
+++ src/Block.java
@@ -1,20 +1,23 @@
-import java.util.ArrayList;
+import java.util.List;
 
 public class Block {
-    private Tool tool;
-    private ArrayList<Integer> coordinates;
+    private final Tool tool;
     Blank blank;
 
-    public Block(Tool tool, ArrayList<Integer> coordinates, Blank blank) {
+    public Block(Tool tool, Blank blank) {
         this.tool = tool;
-        this.coordinates = coordinates;
         this.blank = blank;
     }
 
-    public String createBlockOfCode() {
+    public String createBlockOfCode(List<Integer> coordinates) {
         StringBuilder sb = new StringBuilder(tool.getName() + ";\n");
-        String format = String.format("G97 S%d M03;\nM8;\nG0 G54 X%d. Z2.;\n", tool.getG97(), blank.getD()+2);
+        String format = String.format("G97 S%d M03;\nM8;\nG0 G54 X%d. Z2.;\n",
+                tool.getG97(), blank.getD()+2);
         sb.append(format);
+        for (int i = 0; i < coordinates.size(); i = i + 2) {
+            format = String.format("G1 X%d. Z%d.;\n", coordinates.get(i), coordinates.get(i + 1));
+            sb.append(format);
+        }
         return sb.toString();
     }
 
--- src/Fanuc.java
+++ src/Fanuc.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Fanuc {
     public static void main(String[] args) {
-        Block block = new Block(new Tool(NameOfTool.PROHODNOY), new ArrayList<>(), new Blank(20,20));
-        System.out.println(block.createBlockOfCode());
+        List<Integer> coordinates = new ArrayList<>(Arrays.asList(5,0,7,-1,7,-10,15,-10,19,-12,19,-30));
+        Block block = new Block(new Tool(NameOfTool.PROHODNOY), new Blank(20,20));
+        System.out.println(block.createBlockOfCode(coordinates));
     }
 }
 
